09cddbffd00b96855ffd1862173be2eb53f5d47f
Igor <i.przybylski@zsti.elodz.edu.pl>
2024-03-26 00:07:26 +0100

    index deleted

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,5 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/poly_variants", as: :rails_health_check
-  root "poly_variants#index"
+  root "poly_variants#advanced_search"
 end
